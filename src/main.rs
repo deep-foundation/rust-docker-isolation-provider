701e13908a21a00aa91e1f4bd9a4946fbbcc46d8
uselessgoddess <dodickgod@gmail.com>
2023-08-03 11:50:22 +0300

    What is the hell

--- src/main.rs
+++ src/main.rs
@@ -180,8 +180,9 @@ mod tests {
             })
             .dispatch();
 
-        assert_eq!(res.status(), Status::Ok);
-        assert_eq!(res.into_json::<Value>().unwrap(), json!({ "resolved": "Hi world" }))
+        panic!("{:?}", res.into_string());
+        // assert_eq!(res.status(), Status::Ok);
+        // assert_eq!(res.into_json::<Value>().unwrap(), json!({ "resolved": "Hi world" }))
     }
 
     #[test]
